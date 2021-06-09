3ae890d0d55e735be787504f00e0bdce2b2a7c02
danielbossier <dbossier21@gmail.com>
2021-06-09 11:41:47 -0700

    adding files

---
+++ beer.rb
@@ -0,0 +1,12 @@
+class Beer
+  def intialize(input_color, input_price, input_style)
+    @color = input_color
+    @price = input_price
+    @style = input_style
+  end
+  
+  def print_info
+    puts 
+  end
+  
+end
---
+++ store_item.rb
@@ -0,0 +1,9 @@
+beer1 = {color: "gold", price: 5, style: "belgian triple"}
+beer2 = {color: "red", price: 6, style: "red ale"}
+beer3 = {color: "black", price: 7, style: "stout"}
+
+beer1 = {:color => "gold", :price => 5, :style => "belgian triple"}
+beer2 = {:color => "red", :price => 6, :style => "red ale"}
+beer3 = {:color => "black", :price => 7, :style => "stout"}
+
+p beer1[:color].upcase
