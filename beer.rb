99dcda4c92e92cfa9400c47865af996b80cbc22c
danielbossier <dbossier21@gmail.com>
2021-06-09 12:09:04 -0700

    added attr_reader and attr_writer

--- beer.rb
+++ beer.rb
@@ -1,12 +1,27 @@
 class Beer
-  def intialize(input_color, input_price, input_style)
+  attr_reader :color, :price, :style
+  attr_writer :price
+
+
+  def initialize(input_color, input_price, input_style)
     @color = input_color
     @price = input_price
     @style = input_style
   end
   
   def print_info
-    puts 
+    puts "The product is a #{color} #{style} that costs $#{price}."
   end
   
 end
+
+beer1 = Beer.new("gold", 5, "belgian triple")
+beer2 = Beer.new("red", 6, "red ale")
+beer3 = Beer.new("black", 7, "stout")
+
+beer1.print_info
+beer2.print_info
+beer3.print_info
+
+beer1.price = 7
+beer1.print_info
--- store_item.rb
+++ store_item.rb
@@ -7,3 +7,4 @@ beer2 = {:color => "red", :price => 6, :style => "red ale"}
 beer3 = {:color => "black", :price => 7, :style => "stout"}
 
 p beer1[:color].upcase
+
