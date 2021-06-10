2ae914e24760d7771ad007b20155618e71dc108e
danielbossier <dbossier21@gmail.com>
2021-06-10 12:17:25 -0700

    added annual raise and fire methods

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,12 +18,30 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_accessor :fuel, :make, :model
+
+  def initialize(input_options)
+    super
+    @fuel = fuel
+    @make = "Honda"
+    @model = "Accord"
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  attr_accessor :type, :weight, :wheels
+
+  def initialize(type: "special", weight: "4.9kg", wheels: 2)
+    super
+    @type = type
+    @weight = weight
+    @wheels = wheels
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
