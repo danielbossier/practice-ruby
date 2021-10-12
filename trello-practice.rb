bc19b2ab12da78f44998cca60bccc993b17ebca8
danielbossier <dbossier21@gmail.com>
2021-10-12 10:12:03 -0700

    hash practice

--- trello-practice.rb
+++ trello-practice.rb
@@ -269,18 +269,68 @@ while i < string.length
 return output it will be either "true", or "false"
 =end
 
-def palindrome?(string)
-  output = "true"
-  i = 0
-  while i < string.length
-    if string[0] != string[-(i + 1)]
-      output = "false"
-      return output
-    else
-      i += 1
+# def palindrome?(string)
+#   i = 0
+#   while i < (string.length / 2)
+#     if string[i] != string[-(i + 1)]
+#       return false
+#     else
+#       i += 1
+#     end
+#   end
+#   return true
+# end
+
+# p palindrome?("whatupbutt")
+
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+# menu = { "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 }
+
+# def everything(menu)
+#   total = 0
+#   menu.each do |item, price|
+#     total += price
+#   end
+#   return total
+# end
+
+# p everything({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+def over_one_thousand(array)
+  output = []
+
+  array.each do |hash|
+    if hash[:likes] >= 1000
+      output << hash
     end
   end
   return output
 end
 
-p palindrome?("racecar")
+p over_one_thousand([
+    { title: "Me Eating Pizza", submitted_by: "Joelle P.", likes: 1549 },
+    { title: "i never knew how cool i was until now", submitted_by: "Lyndon Johnson", likes: 3 },
+    { title: "best selfie evar!!!", submitted_by: "Patti Q.", likes: 1092 },
+    { title: "Mondays are the worst", submitted_by: "Aunty Em", likes: 644 },
+  ])
