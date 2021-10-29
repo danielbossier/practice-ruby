d66a36c987417ce13cf112e5c4118f1bf1478949
danielbossier <dbossier21@gmail.com>
2021-10-28 18:27:32 -0700

    ladder

--- trello-practice.rb
+++ trello-practice.rb
@@ -819,3 +819,50 @@ end
 # end
 
 # even_numbers
+
+=begin
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
+
+define a function common_prefix(array)
+i = 0
+output = ""
+while loop i < array[0]
+  if array[0][i] == array[1][i] && array[0][i] == array[2][i]
+    output += array[0][i]
+  end
+  i += 1 
+end
+return output
+end
+
+p common_prefix
+=end
+
+# def common_prefix(array)
+#   i = 0
+#   output = ""
+#   while i < array[0].length
+#     if array[0][i] == array[1][i] && array[0][i] == array[2][i]
+#       output += array[0][i]
+#     end
+#     i += 1
+#   end
+#   return output
+# end
+
+# p common_prefix(["flower", "flow", "flight"])
